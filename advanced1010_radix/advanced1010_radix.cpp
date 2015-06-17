13072ef69819279ee32f1b232bbfc57444d59230
Mirs King <mirsking@gmail.com>
2015-06-17 22:38:29 +0800

    advanced 1010 radix, 1st: max radix is not 36; 2st: use binary search to find radix while not just search, avoid of timeout.

--- advanced1010_radix/advanced1010_radix.cpp
+++ advanced1010_radix/advanced1010_radix.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 /* abcdefg*/
-long long convert2Dec(string str, int radix)
+long long convert2Dec(string str, long long radix)
 {
 	long long res = 0;
 	//radix = (radix >= 'a'&& radix <= 'z') ? (radix - 'a' + 10+1) : radix+1;
 	for (int i = 0; i < str.size(); i++)
 	{
-		int val = (str[i] >= 'a'&& str[i] <= 'z') ? str[i] - 'a'+10 : str[i] - '0';
+		long long val = (str[i] >= 'a'&& str[i] <= 'z') ? str[i] - 'a'+10 : str[i] - '0';
 		res = res * radix + val;
 	}
 	return res;
@@ -19,7 +20,8 @@ long long convert2Dec(string str, int radix)
 int main(int argc, char** argv)
 {
 	string a, b;
-	int tag, radix;
+	int tag;
+	long long radix;
 	cin >> a >> b >> tag >> radix;
 	string aa, bb;
 	if (tag == 1)
@@ -36,21 +38,35 @@ int main(int argc, char** argv)
 	long long real_val = convert2Dec(aa, radix);
 
 	// find bb min radix
-	int min_radix = 2;
+	long long min_radix = 2;
 	for (int i = 0; i < bb.size(); i++)
 	{
-		int val = (bb[i] >= 'a'&& bb[i] <= 'z') ? bb[i] - 'a'+10 : bb[i] - '0';
+		long long val = (bb[i] >= 'a'&& bb[i] <= 'z') ? bb[i] - 'a'+10 : bb[i] - '0';
 		min_radix = (min_radix < val+1) ? val+1 : min_radix;
 	}
 
-	int got_radix = -100;
-	for (int i = min_radix; i <= 36; i++)
+	long long got_radix = -100;
+	long long L = min_radix;
+	long long R = max<long long>(min_radix, real_val+1);
+	long long M;
+	while (L<=R)
 	{
-		if (real_val == convert2Dec(bb, i))
+		M = (L + R) / 2;
+		long long got_val = convert2Dec(bb, M);
+		if (real_val == got_val)
 		{
-			got_radix = i;
+			got_radix = M;
 			break;
 		}
+		else if (got_val < 0 ||real_val < got_val)
+		{
+			R = M - 1;
+		}
+		else if (real_val > got_val)
+		{
+			L = M + 1;
+		}
+
 	}
 	if (got_radix != -100)
 		cout << got_radix;

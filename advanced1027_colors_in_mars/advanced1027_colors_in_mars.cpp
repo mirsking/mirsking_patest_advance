b4aafec118350a6c75f1556e1cc16d71b81daccf
mirsking <mirsking@gmail.com>
2015-07-07 11:18:14 +0800

    advanced 1027 colors in mars

---
+++ advanced1027_colors_in_mars/advanced1027_colors_in_mars.cpp
@@ -0,0 +1,29 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include <vector>
+using namespace std;
+
+char radix[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C'};
+
+vector<char> convertMarsColor(int r, int g, int b)
+{
+	vector<char> res;
+	res.push_back(radix[r / 13]);
+	res.push_back(radix[r % 13]);
+	res.push_back(radix[g / 13]);
+	res.push_back(radix[g % 13]);
+	res.push_back(radix[b / 13]);
+	res.push_back(radix[b % 13]);
+	return res;
+}
+
+int main()
+{
+	int R, G, B;
+	scanf("%d%d%d", &R, &G, &B);
+	//cin >> R >> G >> B;
+	vector<char> res = convertMarsColor(R, G, B);
+	for (auto it = res.begin(); it != res.end(); it++)
+		printf("%c", *it);
+	return 0;
+}

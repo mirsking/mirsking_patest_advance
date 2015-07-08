689ceaf81a1949d497d2d80fb3380719f62a7bef
mirsking <mirsking@gmail.com>
2015-07-08 16:25:55 +0800

    advanced 1029 median, use scanf to avoid time out

--- advanced1029_median/advanced1029_median.cpp
+++ advanced1029_median/advanced1029_median.cpp
@@ -1,3 +1,4 @@
+#pragma warning(disable:4996)
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -6,26 +7,52 @@ using namespace std;
 
 int main()
 {
-	vector<int> a, b;
+	vector<long long> a, b;
 	int n;
-	cin >> n;
+	scanf("%lld", &n);
 	while (n--)
 	{
-		int tmp;
-		cin >> tmp;
+		long long tmp;
+		scanf("%lld", &tmp);
 		a.push_back(tmp);
 	}
-	cin >> n;
+	scanf("%lld", &n);
 	while (n--)
 	{
 		int tmp;
-		cin >> tmp;
+		scanf("%lld", &tmp);
 		b.push_back(tmp);
 	}
+	
+	/*
+	int ma = a[(a.size() - 1) / 2];
+	int mb = b[(b.size() - 1) / 2];
 	vector<int> c;
+	if (ma <= mb)
+	{
+		int i = (a.size() - 1) / 2;
+		while (i < a.size() && a[i] <= mb)
+		{
+			c.push_back(a[i]);
+			i++;
+		}
+		c.push_back(mb);
+	}
+	else 
+	{
+		int i = (b.size()-1)/2;
+		while (i < b.size() && b[i] <= ma)
+		{
+			c.push_back(b[i]);
+			i++;
+		}
+		c.push_back(ma);
+	}
+	*/
+	vector<long long> c;
 	c.resize(a.size() + b.size());
-
 	merge(a.begin(), a.end(), b.begin(), b.end(), c.begin());
 	cout << c[(c.size() - 1) / 2];
+
 	return 0;
 }

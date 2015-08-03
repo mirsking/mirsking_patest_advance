0fa084dbbc26567b0b4ac2704d16cc1871c2a62d
mirsking <mirsking@gmail.com>
2015-08-03 17:41:15 +0800

    advanced 1039 change index from string to int, timeout again

--- advanced1039_course_list_for_students/advanced1039_course_list_for_students.cpp
+++ advanced1039_course_list_for_students/advanced1039_course_list_for_students.cpp
@@ -6,9 +6,9 @@
 #include <algorithm>
 using namespace std;
 
-typedef unordered_set<string> course_t;
+typedef unordered_set<int> course_t;
 
-void queryStu(const vector<course_t>& courses, const string& stu, vector<int>& res)
+void queryStu(const vector<course_t>& courses, const int stu, vector<int>& res)
 {
 	for (size_t i = 0; i < courses.size(); i++)
 	{
@@ -29,23 +29,25 @@ int main()
 		scanf("%d %d\n", &course, &num);
 		while (num--)
 		{
-			string stu; stu.resize(4);
-			scanf("%s ", &stu[0]);
-			courses[course - 1].insert(stu);
+			char stu[4];
+			scanf("%s ", stu);
+			int stu_i = (stu[0] - 'A') * 26 * 26 * 10 + (stu[1] - 'A') * 26 * 10 + (stu[2] - 'A') * 10 + stu[3] - '0';
+			courses[course - 1].insert(stu_i);
 		}
 		scanf("\n");
 	}
 	
 	while (M--)
 	{
-		string stu; stu.resize(4);
+		char stu[4];
 		if (M == 0)
 			scanf("%s", &stu[0]);
 		else
 			scanf("%s ", &stu[0]);
+		int stu_i = (stu[0] - 'A') * 26 * 26 * 10 + (stu[1] - 'A') * 26 * 10 + (stu[2] - 'A') * 10 + stu[3] - '0';
 		vector<int> res;
-		queryStu(courses, stu, res);
-		printf("%s %d", stu.c_str(), res.size());
+		queryStu(courses, stu_i, res);
+		printf("%s %d", stu, res.size());
 		if (res.size() != 0)
 			printf(" ");
 		for (auto it = res.begin(); it != res.end(); it++)

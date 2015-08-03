07cfaf98f9264a9b1da4a45c4fc1f07b535983ec
mirsking <mirsking@gmail.com>
2015-08-03 13:52:42 +0800

    advanced 1039 timeout again

--- advanced1039_course_list_for_students/advanced1039_course_list_for_students.cpp
+++ advanced1039_course_list_for_students/advanced1039_course_list_for_students.cpp
@@ -1,25 +1,28 @@
 #pragma warning(disable:4996)
 #include <iostream>
 #include <unordered_map>
+#include <unordered_set>
 #include <string>
 #include <algorithm>
 using namespace std;
 
-void queryStu(const unordered_multimap<string, int>& stus, const string& stu, vector<int>& course)
+typedef unordered_set<string> course_t;
+
+void queryStu(const vector<course_t>& courses, const string& stu, vector<int>& res)
 {
-	course.clear();
-	auto its = stus.equal_range(stu);
-	for (auto it = its.first; it != its.second;it++)
-		course.push_back(it->second);
-	sort(course.begin(), course.end());
+	for (size_t i = 0; i < courses.size(); i++)
+	{
+		if (courses[i].find(stu) != courses[i].end())
+			res.push_back(i + 1);
+	}
 }
 
 int main()
 {
-	unordered_multimap<string, int> stus;
-
+	vector<course_t> courses;
 	int M, N;
 	scanf("%d %d\n", &M, &N);
+	courses.resize(N);
 	while (N--)
 	{
 		int course, num;
@@ -28,7 +31,7 @@ int main()
 		{
 			string stu; stu.resize(4);
 			scanf("%s ", &stu[0]);
-			stus.insert(make_pair(stu, course));
+			courses[course - 1].insert(stu);
 		}
 		scanf("\n");
 	}
@@ -40,15 +43,15 @@ int main()
 			scanf("%s", &stu[0]);
 		else
 			scanf("%s ", &stu[0]);
-		vector<int> courses;
-		queryStu(stus, stu, courses);
-		printf("%s %d", stu.c_str(), courses.size());
-		if (courses.size() != 0)
+		vector<int> res;
+		queryStu(courses, stu, res);
+		printf("%s %d", stu.c_str(), res.size());
+		if (res.size() != 0)
 			printf(" ");
-		for (auto it = courses.begin(); it != courses.end(); it++)
+		for (auto it = res.begin(); it != res.end(); it++)
 		{
 			printf("%d", *it);
-			if (it + 1 != courses.end())
+			if (it + 1 != res.end())
 				printf(" ");
 		}
 		printf("\n");

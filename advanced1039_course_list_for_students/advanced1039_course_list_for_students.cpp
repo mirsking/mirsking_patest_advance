03b50280160435a651ad63a3efddb9ba791dcd32
mirsking <mirsking@gmail.com>
2015-08-03 18:11:49 +0800

    advanced 1039 course list for students, use priority_queue, ok now

--- advanced1039_course_list_for_students/advanced1039_course_list_for_students.cpp
+++ advanced1039_course_list_for_students/advanced1039_course_list_for_students.cpp
@@ -1,28 +1,17 @@
 #pragma warning(disable:4996)
 #include <iostream>
 #include <unordered_map>
-#include <unordered_set>
 #include <string>
-#include <algorithm>
+#include <queue>
+#include <functional>
 using namespace std;
-
-typedef unordered_set<int> course_t;
-
-void queryStu(const vector<course_t>& courses, const int stu, vector<int>& res)
-{
-	for (size_t i = 0; i < courses.size(); i++)
-	{
-		if (courses[i].find(stu) != courses[i].end())
-			res.push_back(i + 1);
-	}
-}
+typedef unordered_map<int, priority_queue<int, vector<int>, greater<int>>> stu_courses_t;
 
 int main()
 {
-	vector<course_t> courses;
+	stu_courses_t stu_courses;
 	int M, N;
 	scanf("%d %d\n", &M, &N);
-	courses.resize(N);
 	while (N--)
 	{
 		int course, num;
@@ -31,8 +20,18 @@ int main()
 		{
 			char stu[4];
 			scanf("%s ", stu);
-			int stu_i = (stu[0] - 'A') * 26 * 26 * 10 + (stu[1] - 'A') * 26 * 10 + (stu[2] - 'A') * 10 + stu[3] - '0';
-			courses[course - 1].insert(stu_i);
+			int stu_i = (stu[0]-'A') * 26 * 26 * 10 + (stu[1]-'A') * 26 * 10 + (stu[2]-'A') * 10 + stu[3]-'0';
+			auto it = stu_courses.find(stu_i);
+			if (it == stu_courses.end())
+			{	
+				priority_queue<int, vector<int>, greater<int>> tmp;
+				tmp.push(course);
+				stu_courses[stu_i] = tmp;
+			}
+			else
+			{
+				stu_courses[stu_i].push(course);
+			}
 		}
 		scanf("\n");
 	}
@@ -43,21 +42,23 @@ int main()
 		if (M == 0)
 			scanf("%s", &stu[0]);
 		else
-			scanf("%s ", &stu[0]);
-		int stu_i = (stu[0] - 'A') * 26 * 26 * 10 + (stu[1] - 'A') * 26 * 10 + (stu[2] - 'A') * 10 + stu[3] - '0';
-		vector<int> res;
-		queryStu(courses, stu_i, res);
-		printf("%s %d", stu, res.size());
-		if (res.size() != 0)
+			scanf("%s ", stu);
+		int stu_i = (stu[0]-'A') * 26 * 26 * 10 + (stu[1]-'A') * 26 * 10 + (stu[2]-'A') * 10 + stu[3]-'0';
+		priority_queue<int, vector<int>, greater<int>> courses = stu_courses[stu_i];
+		printf("%s %d", stu, courses.size());
+		if (courses.size() != 0)
 			printf(" ");
-		for (auto it = res.begin(); it != res.end(); it++)
+		int count = 0;
+		int max = courses.size();
+		while (!courses.empty())
 		{
-			printf("%d", *it);
-			if (it + 1 != res.end())
+			count++;
+			printf("%d", courses.top());
+			courses.pop();
+			if (count !=max) 
 				printf(" ");
 		}
 		printf("\n");
 	}
 	return 0;
 }
-

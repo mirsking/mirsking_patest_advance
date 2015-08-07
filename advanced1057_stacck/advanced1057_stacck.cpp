e2abc3bfc7f28ea9276f7157864cfdbe52db0043
mirsking <mirsking@gmail.com>
2015-08-07 23:50:13 +0800

    advanced 1057 stack, use binary index tree, don't know why and don't know whether is right.

--- advanced1057_stacck/advanced1057_stacck.cpp
+++ advanced1057_stacck/advanced1057_stacck.cpp
@@ -4,13 +4,58 @@
 #include <string>
 using namespace std;
 
+struct BinTree{
+	static const int MAXN = 100001;
+	vector<int> vec;
+
+	BinTree(){ vec = vector<int>(MAXN, 0); }
+
+	int lowbit(int x)
+	{
+		return x&(-x);
+	}
+
+	void update(int x, int val)
+	{
+		while (x <= MAXN)
+		{
+			vec[x] += val;
+			x += lowbit(x);
+		}
+	}
+
+	int getSum(int x)
+	{
+		int sum = 0;
+		while (x > 0)
+		{
+			sum += vec[x];
+			x -= lowbit(x);
+		}
+		return sum;
+	}
+
+	int findMedian(int val, int l = 0, int r = MAXN - 1)
+	{
+		if (l == r)
+			return l;
+
+		int mid = (l + r) / 2;
+		if (getSum(mid) < val)
+			return findMedian(val, mid + 1, r);
+		else
+			return findMedian(val, l, mid);
+	}
+
+};
+
 class MyStack
 {
 public:
 	void push(int val)
 	{
 		sta.push_back(val);
-		vec.push_back(val);
+		bin_tree.update(val, 1);
 	}
 	void pop()
 	{
@@ -19,8 +64,8 @@ public:
 		else
 		{
 			cout << *sta.rbegin() << endl;
+			bin_tree.update(*sta.rbegin(), -1);
 			sta.pop_back();
-			vec.pop_back();
 		}
 	}
 	void peakMedian()
@@ -29,13 +74,12 @@ public:
 			cout << "Invalid" << endl;
 		else
 		{
-			sort(vec.begin(), vec.end());
-			cout << vec[(vec.size() - 1) / 2] << endl;
+			cout << bin_tree.findMedian((sta.size()+1)/2) << endl;
 		}
 	}
 private:
 	vector<int> sta;
-	vector<int> vec;
+	BinTree bin_tree;
 };
 
 
@@ -48,14 +92,20 @@ int main()
 	{
 		string str;
 		cin >> str;
-		if (str == "Pop")
-			sta.pop();
-		else if (str == "PeekMedian")
-			sta.peakMedian();
-		else if (str == "Push")
+		switch (str[1])
 		{
-			int val; cin >> val;
-			sta.push(val);
+			case 'o':
+				sta.pop();
+				break;
+			case 'e':
+				sta.peakMedian();
+				break;
+			case 'u':
+			{
+				int val; cin >> val;
+				sta.push(val);
+				break;
+			}
 		}
 	}
 	return 0;

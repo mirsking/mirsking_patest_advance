2f5721dd58a5f4e727eb8254bcb6c7f69aa302d4
mirsking <mirsking@gmail.com>
2015-08-07 16:27:02 +0800

    advanced 1054 the dominant color, test point 5 error

---
+++ advanced1054_the_dominant_color/advanced1054_the_dominant_color.cpp
@@ -0,0 +1,38 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+
+int main()
+{
+
+	unordered_map<int, int> colors;
+	int M, N;
+	scanf("%d%d", &M, &N);
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			int tmp;
+			scanf("%d", &tmp);
+			if (colors.find(tmp) != colors.end())
+				colors[tmp]++;
+			else
+				colors[tmp] = 0;
+		}
+	}
+	int max_count = 0, max_color = -1;
+	for (auto it = colors.begin(); it != colors.end();it++)
+	{
+		if (it->second > max_count)
+		{
+			max_count = it->second;
+			max_color = it->first;
+		}
+	}
+	if (max_color != -1)
+		cout << max_color;
+	return 0;
+}

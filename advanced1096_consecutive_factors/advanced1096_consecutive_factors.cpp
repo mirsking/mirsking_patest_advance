620b8afe371011f7a9eabeed993021a5e4e6bfc7
mirsking <mirsking@gmail.com>
2015-08-16 20:28:46 +0800

    advanced 1096 consecutive factors, test point 3 4 error, test point 6 timeout

---
+++ advanced1096_consecutive_factors/advanced1096_consecutive_factors.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int n_bak = n;
+	int dbegin = n, dend = n+1;
+	int begin = 2, end = 2;
+	while (n>1)
+	{
+		if (n%end == 0)
+		{
+			n = n / end;
+			end++;
+		}
+		else
+		{
+			if (end - begin > dend - dbegin)
+			{
+				dbegin = begin;
+				dend = end;
+			}
+			begin = begin + 1;
+			end = begin;
+			n = n_bak;
+		}
+	}
+	cout << dend - dbegin << endl;
+	for (int i = dbegin; i < dend; i++)
+	{
+		cout << i;
+		if (i != dend - 1)
+			cout << '*';
+	}
+	return 0;
+}
